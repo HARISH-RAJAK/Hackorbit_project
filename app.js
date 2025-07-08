16745db12da59705a698d5a4ccae2bb9cc15013f
HARISH-RAJAK <rajakharish027@gmail.com>
2025-07-09 02:02:57 +0530

    updated server

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
-
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+var cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
- res.render('landing.ejs');
-});
+app.use(expressSession({
+    secret: 'harish_is_the_king',
+    resave: false,
+    saveUninitialized: false,
+}));
 
-app.get('/login', (req, res) => {
- res.render('login.ejs');
-});
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(userRouter.serializeUser());
+passport.deserializeUser(userRouter.deserializeUser());
 
-app.get('/register', (req, res) => {
- res.render('register.ejs');
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));  
 
-app.get('/home', (req, res) => {
- res.render('home.ejs');
-});
+app.use('/', indexRouter);  
 
-app.get('/renting',(req,res)=>{
-    res.render('renting.ejs');
-});
 
-app.get('/cropinfo',(req,res)=>{
-    res.render('cropinfo.ejs');
-});
-
-app.get('/schemes',(req,res)=>{
-    res.render('schemes.ejs');
-});
-
-app.get('/importexport',(req,res)=>{
-    res.render('importExport.ejs');
-});
-
-app.get('/profile',(req,res)=>{
-    res.render('profile.ejs');
-});
-
-app.get('/workers',(req,res)=>{
-    res.render('workers.ejs');
-});
 
 app.listen(3000, () => {
     console.log('server is running on port 3000');
